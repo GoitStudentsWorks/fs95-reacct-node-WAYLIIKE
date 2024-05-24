b1c594198f6669485ce1958c1acb5df2efecb5de
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-05-25 00:14:14 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/watertrack-project-nodejs-react">
+        <BrowserRouter basename="/fs95-reacct-node-WAYLIIKE">
           <HelmetProvider>
             <App />
           </HelmetProvider>
